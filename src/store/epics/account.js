3584f82bbfbd50fce491da62a670c414ad7bb26f
LucasBadico <lucasgomesdesantana@gmail.com>
2018-08-09 08:34:49 -0300

    async modal working

--- src/store/epics/account.js
+++ src/store/epics/account.js
@@ -3,24 +3,24 @@ import { ofType } from 'redux-observable';
 import { filter, mapTo, switchMap, mergeMap, map, flatMap } from 'rxjs/operators';
 import { fetchData } from '../../utils'
 
-export const getListAccounts = (action$, store) => action$.pipe(
+export const fetchAccounts = (action$, store) => action$.pipe(
     ofType('WILL_LOAD_LIST'),
-    switchMap(() => 
-        fetchData({
-                    // this still a mock
-                    url: 'https://dev-api.stefanini-spark.com/accounts?pageSize=5'
-                }).pipe(
-                    map(({ response }) => ([
-                        {
-                            type: 'OPEN_FORM',
-                            form: 'list',
-                        }
-                        ,{
-                        type: 'REQUEST_FULFILLED',
-                        on: 'list',
-                        data: response,
-                    }])),
-                    flatMap(from)
-                )
+    switchMap(() => fetchData({
+            url: 'https://dev-api.stefanini-spark.com/accounts?pageSize=5'
+        }).pipe(
+            map(({ response }) => ({
+                type: 'REQUEST_FULFILLED',
+                on: 'list',
+                data: response,
+            })),
+            //flatMap(from)
+        ))
     )
+
+export const openModal = (action$, store) => action$.pipe(
+    ofType('WILL_LOAD_LIST'),
+    mapTo({
+        type: 'OPEN_FORM',
+        form: 'list',
+    })
 )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ import { ajax } from 'rxjs/ajax'
 import { of, from } from 'rxjs'
 import { filter, map, tap, delay } from 'rxjs/operators'
 import * as R from 'ramda'
-import { mock } from './mock' 
+
 export const fetchData = (
     url,
     method='GET',
@@ -11,7 +11,9 @@ export const fetchData = (
     headers={
     'Content-Type': 'application/json'
     }
-) => of({ response: mock }).pipe(
-    delay(1000)
-)
+) => ajax({
+    url : 'https://dev-api.stefanini-spark.com/accounts?pageSize=10',
+    crossDomain: true,
+  })
+
 // TODO Add a swithMap here to r
